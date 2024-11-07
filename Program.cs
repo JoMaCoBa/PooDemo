6ce2adddfbfcb9c6349d17cbdecfbcb57a63a4da
JoMaCoBa <correabajo@gmail.com>
2024-11-06 19:50:55 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Smartphone oneplus = new Smartphone();
+
+class Smartphone
+{
+
+    public string brand;
+    public string model;
+    public string color;
+    public byte ram;
+    public int storage;
+
+}
